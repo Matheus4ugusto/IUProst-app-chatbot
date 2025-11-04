11bdadcf13088ad5dcb77819a5e9273108159219
Matheus4gusto <matheuspaulasoares10@gmail.com>
2025-11-04 19:13:46 -0300

    Inicia desenvolvimento do layout do aplicativo

--- components/chat/chat.styles.ts
+++ components/chat/chat.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  justify-content: center;
+  gap: 16px;
   align-items: center;
   background-color: #221f1fff;
 `;
@@ -12,3 +12,41 @@ export const Text = styled.Text`
   color: #ffffff;
   font-size: 18px;
 `;
+
+export const Input = styled.TextInput`
+  width: 80%;
+  background-color: #474242ff;
+  padding: 12px 6px;
+  outline: none;
+  border: solid 0px;
+  color: white;
+`;
+
+export const SendButtonTouchable = styled.TouchableOpacity`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #474242ff;
+  padding: 8px;
+  margin-right: 8px;
+  border-radius: 8px;
+`
+
+export const InputContainer = styled.View`
+  width: 80%;
+  background-color: #474242ff;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 16px;
+  padding: 8px;
+`
+
+export const HistoryView = styled.View`
+  flex: 1;
+  width: 80%;
+  border-radius: 16px;
+  background-color: #474242ff;
+`
--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -1,9 +1,28 @@
+import { Keyboard, KeyboardAvoidingView, Platform, Text, TouchableWithoutFeedback } from "react-native";
+import Icon from 'react-native-vector-icons/AntDesign';
 import * as S from './chat.styles';
 
 export default function Chat() {
-    return(
-    <S.Container>
-      <S.Text>Olá, mundo</S.Text>
-    </S.Container>
-    )
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: "#474242ff" }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 65 : 0} // ajuste se houver header
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <S.Container>
+          <S.HistoryView>
+            <Text>Olá, mundo!</Text>
+          </S.HistoryView>
+
+          <S.InputContainer>
+            <S.Input placeholder='Escreva aqui...' />
+            <S.SendButtonTouchable onPress={() => {/* handle send */ }} accessibilityLabel="Enviar">
+              <Icon name="send" size={25} color={'#fff'} />
+            </S.SendButtonTouchable>
+          </S.InputContainer>
+        </S.Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  )
 }
